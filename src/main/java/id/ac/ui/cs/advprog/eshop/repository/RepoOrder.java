749e4d7040b9a7e27ab7c08bde1d0ddee87b0902
HH <edricsuselo04@gmail.com>
2025-05-23 03:48:24 +0700

    [RED] RepoOrder Base

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/RepoOrder.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepoOrder {
+
+    private List<Order> orderData = new ArrayList<>();
+
+    public Order save(Order o) {return null;}
+
+    public Order findById(String id) {return null;}
+
+    public List<Order> findAllByAuthor(String a) {return null;}
+}
